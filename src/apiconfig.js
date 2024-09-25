31fbc3b7519c88b5071fb46dfe1c5f792bbfed38
Anisha Rani <2021pietcsanisha021@poornima.org>
2024-09-25 23:25:24 +0530

    Update apiconfig.js

--- src/apiconfig.js
+++ src/apiconfig.js
@@ -1,2 +1,3 @@
-// export const API = 'https://mern-connectx-backend-3.onrender.com';
-export const API = 'http://localhost:5000';
+export const API = 'https://connectx-server-4.onrender.com';
+// export const API = 'http://localhost:5000';
+  // const apiUrl = process.env.NODE_ENV === 'production' ? 'https://connectx-server-4.onrender.com' : 'http://localhost:5000';
